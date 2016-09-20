c451fff02a19864b846135728a14ae491191ce5d
Mason Seaman <masonseaman@gmail.com>
2016-09-19 22:03:42 -0400

    Update trends.js

--- trends.js
+++ trends.js
@@ -7,7 +7,7 @@ var trends = [
 				{name: "trend5", coordinates: "40.7127837,-74.00594130000002"}
 			];
 			
-document.getElementById('trends').addEventListener("click", show);
+index.getElementById('trends').addEventListener("click", show);
 
 function show()
 {
