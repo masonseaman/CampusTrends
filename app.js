d763d3ba2bb5337ba3395b50a56d524ce0158a59
Mason Seaman <masonseaman@gmail.com>
2016-10-27 02:14:25 -0400

    test

--- app.js
+++ app.js
@@ -1,35 +1,6 @@
-var firebase = require("firebase");
+var http = require('http')
 
-var express = require('express');
-
-var app = express();
-
-var bodyParser = require('body-parser')
-
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-
-    extended: true
-
-}));
-
-firebase.initializeApp({
-
-      apiKey: "AIzaSyAWD7noPfQsrc0mjq9KJgGRvqJWWYXQ7TI",
-            authDomain: "campus-trends.firebaseapp.com",
-            databaseURL: "https://campus-trends.firebaseio.com",
-            storageBucket: "campus-trends.appspot.com",
-            messagingSenderId: "425903145524"
-
-});
-
-var fireRef = firebase.database().ref('todos');
-
-var port = process.env.PORT || 3000;
-
-app.listen(port, function () {
-
-    console.log('Example app listening on port ' + port);
-
-});
+http.createServer(function (request, response){
+	response.writeHead(200, {"Content-Type": "text/plain"})
+	response.end("Hello World\n")
+}).listen(proces.env.PORT)
