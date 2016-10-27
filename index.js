8a1326af273220bc7c0cfb88b9fc8401bd8097b5
Mason Seaman <masonseaman@gmail.com>
2016-10-27 02:55:32 -0400

    update

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-
-var firebase = require("firebase");
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-}));
 app.use(express.static('public'));
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
