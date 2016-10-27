67c928b35603c4216e979b6a1761f2ef6bb8b306
Mason Seaman <masonseaman@gmail.com>
2016-10-27 03:43:12 -0400

    add privkey

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
+
+firebase.initializeApp({
+	serviceAccount: "privkey.json",
+	databaseURL: "https://campustrends.firebaseio.com"
+});
+
+var fireRef = firebase.database().ref('todos');
+
 app.use(express.static('public'));
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
