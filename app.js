9c70e7c0ecf8c6975ec61841df0017ff0267e7d8
Mason Seaman <masonseaman@gmail.com>
2016-10-27 02:24:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
-var port = process.env.PORT || 3000;
 
-var http = require('http');
+var express = require('express');
 
-http.createServer(function (request, response){
-	response.writeHead(200, {"Content-Type": "text/plain"})
-	response.end("Hello World\n")
-}).listen(proces.env.PORT);
+var app = express();
+
+
+
+app.set('port', (process.env.PORT || 5000));
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+// views is directory for all template files
+
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', function(request, response) {
+
+  response.render('pages/index');
+
+});
+
+
+
+app.listen(app.get('port'), function() {
+
+  console.log('Node app is running on port', app.get('port'));
+
+});
+
+
+
+
