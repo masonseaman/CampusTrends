a6d0eeec8c71fd66eeb9610bd096e4d9f72af48a
Mason Seaman <masonseaman@gmail.com>
2016-10-27 02:06:13 -0400

    fix app.js

--- app.js
+++ app.js
@@ -14,107 +14,19 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 }));
 
-
-
 firebase.initializeApp({
 
-    serviceAccount: "privkey.json",
-
-    databaseURL: "https://swe432lecture12.firebaseio.com"
+      apiKey: "AIzaSyAWD7noPfQsrc0mjq9KJgGRvqJWWYXQ7TI",
+            authDomain: "campus-trends.firebaseapp.com",
+            databaseURL: "https://campus-trends.firebaseio.com",
+            storageBucket: "campus-trends.appspot.com",
+            messagingSenderId: "425903145524"
 
 });
 
 var fireRef = firebase.database().ref('todos');
 
-
-
 var port = process.env.PORT || 3000;
-
-
-
-app.post('/newTodo', function (req, res) {
-
-    console.log("New req");
-
-    console.log("Client wants to create todo: '" + req.body.todoText + "'");
-
-    fireRef.push({"text": req.body.todoText}, function () {
-
-        res.send("OK!");
-
-    }).catch(function(){
-
-        res.status(403);
-
-    });
-
-});
-
-app.post('/editTodo', function (req, res) {
-
-    console.log("Client wants to update todo: '" +req.body.key+ " To " + req.body.todoText + "'");
-
-    if(req.body.todoText.toLowerCase().includes("lasagna"))
-
-        res.status(403);
-
-    else
-
-        fireRef.child(req.body.key).set({"text": req.body.todoText}, function () {
-
-            res.send("OK!");
-
-        }).catch(function(){
-
-            res.status(403);
-
-        });
-
-});
-
-app.post('/deleteTodo', function (req, res) {
-
-    console.log("Client wants to delete todo: '" +req.body.key);
-
-    fireRef.child(req.body.key).once("value", function(item){
-
-        if(item.val().text.toLowerCase().includes("lasagna"))
-
-            res.status(403);
-
-        else
-
-        {
-
-            fireRef.child(req.body.key).remove();
-
-            res.send("OK!");
-
-        }
-
-    }).catch(function(){
-
-        res.status(403);
-
-    });
-
-});
-
-app.get('/emptyHtml.html', function (req, res) {
-
-    console.log("Requested empty html");
-
-    res.send("OK!");
-
-});
-
-
-
-
-
-app.use(express.static('public'));
-
-
 
 app.listen(port, function () {
 
