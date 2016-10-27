b90c37dd465e7fac557b37b22f78ec8626e5460f
Mason Seaman <masonseaman@gmail.com>
2016-10-27 02:53:56 -0400

    index

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
-
-app.get('/emptyHtml.html', function (req, res) {
-    console.log("Requested empty html");
-    res.send("OK!");
-});
 app.use(express.static('public'));
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
