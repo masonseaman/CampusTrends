c361c9152098d8901caeb70a932d9ea73d49c0be
Mason Seaman <masonseaman@gmail.com>
2016-10-27 02:16:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var http = require('http')
+var port = process.env.PORT || 3000;
+
+var http = require('http');
 
 http.createServer(function (request, response){
 	response.writeHead(200, {"Content-Type": "text/plain"})
 	response.end("Hello World\n")
-}).listen(proces.env.PORT)
+}).listen(proces.env.PORT);
